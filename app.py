62ca2944ff7f4823591bd7c4f672f202a6fa9845
l22590343 <l22590343@sjuanrio.tecnm.mx>
2026-05-28 11:26:18 -0600

    fix: agregar ruta raiz

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from config import get_connection
 
 app = Flask(__name__)
 CORS(app)
+@app.route('/')
+def index():
+    return {'mensaje': 'API OXXO funcionando'}
 
 # ══════════════════════════════════════
 # PRODUCTOS
